7cd29dcf33256c3850578791f3edb5f3862198e2
Surukunti Nuthan Reddy <117555349+Nuthan-Reddy-Surukunti@users.noreply.github.com>
2024-08-21 13:28:34 +0530

    Create main.kt

---
+++ main.kt
@@ -0,0 +1,68 @@
+
+
+fun main()
+{
+//   val a =10
+//    val b = 120*5
+//    val result = "sum of $a and $b is ${a+b}"
+//    println(result)
+
+//    val letter: Char = 'A'
+//    print("$letter")
+//    print('\n')
+//    print('\$')
+//    print('\\')
+//    val x:String="Nuthan\n"
+//    var y:String="""Nuthan
+//        |        nuthan
+//       |
+//        |        nuthan
+//    """.trimMargin()
+//    print(y)
+
+    val num = arrayOf(1,2,3,4,5)
+    val words: Array<String> = arrayOf("Kotlin")
+
+    val firstNumber = num[0]
+    val secondWord = words[1]
+    println("First number: $firstNumber")
+    println("second number: $secondWord")
+    num[0]= 10
+    print(num)
+    for (num in )
+
+}
+
+
+fun main()
+{
+//   val a =10
+//    val b = 120*5
+//    val result = "sum of $a and $b is ${a+b}"
+//    println(result)
+
+//    val letter: Char = 'A'
+//    print("$letter")
+//    print('\n')
+//    print('\$')
+//    print('\\')
+//    val x:String="Nuthan\n"
+//    var y:String="""Nuthan
+//        |        nuthan
+//       |
+//        |        nuthan
+//    """.trimMargin()
+//    print(y)
+
+    val num = arrayOf(1,2,3,4,5)
+    val words: Array<String> = arrayOf("Kotlin")
+
+    val firstNumber = num[0]
+    val secondWord = words[1]
+    println("First number: $firstNumber")
+    println("second number: $secondWord")
+    num[0]= 10
+    print(num)
+    for (num in )
+
+}
